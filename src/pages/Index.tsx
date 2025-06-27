1be44ec40afc29d1c4293b7a4b25b6251f7b596a
Mahinaldo <mahinisntfr@gmail.com>
2025-06-27 23:52:20 +0600

    github link added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -505,10 +505,12 @@ const Index = () => {
             <span>by Mahin</span>
           </div>
           <div className="flex items-center justify-center gap-4">
+            <a href="https://github.com/Mahinaldo/micro-tool-lab" target="_blank">
             <Button variant="ghost" size="sm" className="text-gray-500 hover:text-gray-700">
               <Github className="w-4 h-4 mr-2" />
               View Source
             </Button>
+            </a>
           </div>
         </div>
       </div>
